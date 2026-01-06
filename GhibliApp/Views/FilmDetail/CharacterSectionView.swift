a34550e8f9dca64abf3e0a3423b3cf5be703a445
Lucas Nascimento <17522772+lucabelezal@users.noreply.github.com>
2026-01-06 16:17:49 -0300

    feat: add project

--- GhibliApp/Views/FilmDetail/CharacterSectionView.swift
+++ GhibliApp/Views/FilmDetail/CharacterSectionView.swift
@@ -11,32 +11,35 @@ struct CharacterSectionView:  View {
                     .font(.headline)
                 
                 switch viewModel.state {
-                    case .idle: EmptyView()
-                    case .loading:  ProgressView()
+                case .idle:
+                    EmptyView()
+                    
+                case .loading:
+                    ProgressView()
+                    
+                case .loaded(let people):
+                    ForEach(people) { person in
                         
-                    case .loaded(let people):
-                        ForEach(people) { person in
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(person.name)
                             
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(person.name)
-                                
-                                HStack(spacing: 8) {
-                                    Label(person.gender, systemImage: "person.fill")
-                                    Text("Age: \(person.age)")
-                                    Spacer()
-                                    Label(person.eyeColor, systemImage: "eye")
-                                    Text("Hair: \(person.hairColor)")
-                                }
-                                .foregroundColor(.secondary)
-                                .font(.caption)
-                                .lineLimit(1)
-                                
+                            HStack(spacing: 4) {
+                                Label(person.gender, systemImage: "person.fill")
+                                Text("Age: \(person.age)")
+                                Spacer()
+                                Label(person.eyeColor, systemImage: "eye")
+                                Text("Hair: \(person.hairColor)")
                             }
+                            .foregroundColor(.secondary)
+                            .font(.caption)
+                            .lineLimit(1)
+                            
                         }
-                        
-                    case .error(let error):
-                        Text(error)
-                            .foregroundStyle(.pink)
+                    }
+                    
+                case .error(let error):
+                    Text(error)
+                        .foregroundStyle(.pink)
                 }
             }
         }
